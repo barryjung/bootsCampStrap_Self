0ebd7656330fb1e0b46a78af1d73c0170d20a28a
barryjung <wjdgusrbs5@gmail.com>
2023-04-20 17:16:29 +0900

    Update to login,logout function

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('signup/', views.signup_view, name="signup"),
+    path('login/', LoginView.as_view(template_name="user/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
