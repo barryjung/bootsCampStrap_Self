bf3fffe05a0e773b67f74a940ae6df738861eafe
barryjung <wjdgusrbs5@gmail.com>
2023-04-20 19:57:30 +0900

    Update tweet detail&update&delete function

--- tweet/urls.py
+++ tweet/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('feed/', views.feed_view, name="feed"),
     path('create/', views.tweet_create_view, name="tweet_create"),
+    path('detail/<int:id>', views.tweet_detail_view, name="tweet_detail"),
+    path('update/<int:id>', views.tweet_update_view, name="tweet_update"),
+    path('delete/<int:id>', views.tweet_delete_view, name="tweet_delete"),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -23,3 +23,29 @@ def tweet_create_view(request):
             form = TweetForm()
             return render(request, 'tweet/tweet_create.html', {"form": form})
         return redirect('/')
+
+
+def tweet_detail_view(request, id):
+    tweet = TweetModel.objects.get(id=id)
+    return render(request, 'tweet/tweet_detail.html', {"tweet": tweet})
+
+
+def tweet_update_view(request, id):
+    tweet = TweetModel.objects.get(id=id)
+    if request.method == "GET":
+        form = TweetForm(instance=tweet)
+        return render(request, 'tweet/tweet_update.html', {"form": form})
+    elif request.method == "POST":
+        form = TweetForm(request.POST, instance=tweet)
+        if form.is_valid():
+            form.save()
+        else:
+            form = TweetForm()
+            return render(request, 'tweet/tweet_update.html', {"form": form})
+        return redirect('/')
+
+
+def tweet_delete_view(request, id):
+    tweet = TweetModel.objects.get(id=id)
+    tweet.delete()
+    return redirect('/')
